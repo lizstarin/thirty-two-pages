33ca27400628a26fab9b18ef7514260f1f03b961
Liz Starin <liz.starin@gmail.com>
2013-06-03 14:05:03 -0400

    moves some things around

--- ThirtyTwoPages/config/initializers/secret_token.rb
+++ ThirtyTwoPages/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ThirtyTwoPages::Application.config.secret_token = 'd73bc7450d3ff4a284e8305d8e5ee78ac22f545200ebe55c73ce9b6f828c54a658bbf2e10a8da7a090d1d9ce67d64d8b941f680961b62de830ce67110354e4d1'
+ThirtyTwoPages::Application.config.secret_token = '51fdb5b7662fd73f5dffb7977828f338e1853646f5107e3a5f85f806bdf2c9d32220b560ea373161baf80e6a65b85a305ea392c673bf1613513bfc25ad8d1572'
--- ThirtyTwoPages/db/migrate/20130603150131_create_friendships.rb
+++
@@ -1,9 +0,0 @@
-class CreateFriendships < ActiveRecord::Migration
-  def change
-    change_table do |t|
-      t.integer :user_id
-
-      t.timestamps
-    end
-  end
-end
