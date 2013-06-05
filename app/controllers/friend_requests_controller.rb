3d4a36af3ec1b88633fbc2f19b847df6182e728e
Liz Starin <liz.starin@gmail.com>
2013-06-05 00:14:31 -0400

    working on friendship request update

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,16 +1,16 @@
 class FriendRequestsController < ApplicationController
 
   def index
-    @requests_rec = FriendRequest.where(:recipient_id => current_user.id, :status => nil)
-    @requests_sent = FriendRequest.where(:sender_id => current_user.id, :status => nil)
+    @requests_rec = FriendRequest.where(:recipient_id => current_user.id, :status => "pending")
+    @requests_sent = FriendRequest.where(:sender_id => current_user.id, :status => "pending")
   end
 
   def create
     @friend_request = FriendRequest.new(
             :sender_id => current_user.id,
             :recipient_id => params[:user_id],
-            :status => "made"
-            )
+            :status => "pending"
+    )
     if @friend_request.save
       user = User.find(params[:user_id])
       redirect_to user_url(user)
@@ -21,10 +21,13 @@ class FriendRequestsController < ApplicationController
 
   def update
     @friend_request = FriendRequest.where(
-            :sender_id => current_user.id,
-            :recipient_id => params[:user_id],
-            :status => "made"
-            )
+            :sender_id => params[:sender_id],
+            :recipient_id => current_user.id,
+            :status => "pending"
+    )[0]
+		@friend_request.update_attributes(:status => params[:status])
+		@friend_request.save
+		redirect_to user_url(current_user)
   end
 
 end
