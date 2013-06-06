44fcfd2e1347a4a5a92dd332d5f2854a79c5e8da
Liz Starin <liz.starin@gmail.com>
2013-06-06 10:06:12 -0400

    work work work

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
 
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.order("id DESC")
     @notifications.update_all(:read => true)
   end
 
