69901bd4c3a85f0d856f9856094f683fa34d8277
Liz Starin <liz.starin@gmail.com>
2013-06-10 18:17:02 -0400

    work work work on dummy creation

---
+++ app/assets/javascripts/dummy_new.js
@@ -0,0 +1,16 @@
+window.D = {
+
+  $(document).ready(function () {
+
+    $("#dummy").on("make_dummy", function(dummy) {
+        $(".dummy-page").css({
+          "width": dummy.trim_width + "px";
+          "height": dummy.trim_height + "px";
+          "border": "2px solid #ccc";
+        });
+      }
+    )
+
+  });
+
+};
